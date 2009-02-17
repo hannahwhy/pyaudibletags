8a19b2a1a9377a73972b3efdbddf30369624fb76
pjakeating <pjakeating@18cfbfc6-fd23-11dd-b507-8b4ecd5210be>
2009-02-17 22:05:57 +0000

    Initial project construction

---
+++ testpyaudibletags.py
@@ -0,0 +1,15 @@
+#Sample usage:
+import os.path
+from pyaudibletags import aafile
+for root, dirs, files in os.walk(r"F:\Documents\Audio Books\Kay Kenyon\The Entire and the Rose"):
+    for name in files:
+        if not name.endswith(".aa"):
+            continue
+        fullname = os.path.join(root,name)
+        aa = aafile(fullname)
+        print name
+        for d in (aa.tags(),):
+            for k,v in d.items():
+                print "%-32s%r" % (k, v)
+        
+
