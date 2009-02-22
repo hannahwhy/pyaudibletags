3c252c3c8cc491f438c62626462cb2764e6d705a
pjakeating <pjakeating@18cfbfc6-fd23-11dd-b507-8b4ecd5210be>
2009-02-22 13:27:02 +0000

    High-level attribute interface added, with missing data supplied from other fields, multivalued fields such as author separated out into tuples, dates as dates, and strings as unicode.

--- trunk/pyaudibletags.py
+++ trunk/pyaudibletags.py
@@ -1,4 +1,4 @@
-# $Id:$
+# $Id$
 
 # Audiobook files in .aa format from Audible Inc contain content tags for such things as author, title, narrator;
 # information that is otherwise grandly called metadata.
@@ -24,6 +24,9 @@
 
 import os
 import struct
+import datetime
+import time
+import re
 
 # The table of contents describes the layout of the file as triples of integers (<section>, <offset>, <length>).
 # The method toc() returns the following string translations of <section> where these are known.
@@ -34,7 +37,19 @@ locations =      {0: "Whole file",
                   10:"Audio",
                   11:"Cover art"}
 
+partindication = re.compile(",? *Part *\d+ *$", re.IGNORECASE) 
+copyright = re.compile("&#169; *|\(C\);? *")              # Unicode 00A9
+mechanical_copyright = re.compile("&#169; *|\(P\);? *")   # Unicode 2117
+etal = re.compile(',? *(and|with) more *$')
+AandB = re.compile(r'\band\b')
+
 class aafile():
+    
+    __slots__ = ['short_title','user_alias','category_subcategory_search','pubdate','narrator',
+                 'parent_title', 'keywords', 'long_description', 'copyright', 'title', 'is_aggregation',
+                 'parent_id', 'codec', 'provider', 'short_description', 'aggregation_id', 'description',
+                 'price', 'product_id', 'pub_date_start', 'country', 'title_id', 'author', 
+                 'category_search' ]
 
     def __init__(self, afile):
         self.__toc = ()
@@ -43,9 +58,9 @@ class aafile():
             self.ossize = os.stat(self.filename).st_size
             self.f = file(self.filename,'rb')
             try:
-              self.__tags = self.__get_headers(self.f)
+                self.__tags = self.__get_headers(self.f)
             finally:
-              self.f.close()
+                self.f.close()
         elif type(afile) is file:
             if afile.closed:
                 raise IOError, "Cannot analyze a closed file" 
@@ -76,9 +91,9 @@ class aafile():
            and laid out exactly as they appear in the first (usually) 192 bytes of the file.
         """
         if formatted:
-          return ("%13d%13d%13d%13d\n"  + "%13d%13d%13d\n"*self.__toc[2] + "%13d%13d%13d%13d\n" * 2) % self.__toc
+	    return ("%13d%13d%13d%13d\n"  + "%13d%13d%13d\n"*self.__toc[2] + "%13d%13d%13d%13d\n" * 2) % self.__toc
         else:
-          return self.__toc[:]
+            return self.__toc[:]
 
     def tags(self):    
         """Returns a dictionary of around 30 tags describing the audio content of the file
@@ -145,3 +160,217 @@ class aafile():
         if len(tags) != expected_tags:
             raise RuntimeError, 'Expecting %d tags from header, got %d' % (expected_tags, len(tags))
         return tags
+    
+    def __nullcheck(self, key, default=None):
+	try:
+	    tag = self.__tags[key]
+	    if not tag:
+		return default
+	    return unicode(tag, 'latin-1', 'replace')
+	except KeyError:
+	    return default
+
+    def get_short_title(self):
+	try:
+	    return unicode(self.__tags['Content:short_title'].strip(),'latin-1','replace')
+	except IndexError:
+	    return self.title
+	
+    def get_user_alias(self):
+	return self.__nullcheck('Content:user_alias')
+
+    def get_category_subcategory_search(self):
+        cats = tuple([element.strip() for element in self.__nullcheck('Content:category_subcategory_search', '').split(',')])
+	if cats == ('',):
+	    return self.category_search
+
+    def get_pubdate(self):
+	try:
+	    return datetime.date(*(time.strptime(self.__tags['Content:pubdate'], '%d-%b-%Y')[0:3]))
+	except ValueError:
+	    return datetime.date(*(time.strptime(self.__tags['Content:pubdate'], '%d-%b-%y')[0:3]))
+	except IndexError:
+	    return None
+
+    def get_narrator(self):
+	narrator = etal.sub("...",self.__tags['Content:narrator'])
+	narrator = AandB.sub(",",narrator)
+	return tuple([unicode(element.strip(), 'latin-1', 'replace') for element in narrator.split(',')])
+
+    def get_parent_title(self):
+	try:
+	    ptitle = unicode(self.__tags['Content:parent_title'].strip(), 'latin-1', 'replace')
+	except IndexError:
+	    ptitle = None
+	if ptitle:
+	    return ptitle
+	else:
+	    return partindication.sub("", self.title, 1)
+	    
+    def get_keywords(self):
+	try:
+	    return tuple([unicode(element.strip(),'latin-1','replace') for element in self.__tags['Content:keywords'].split(',')])
+	except IndexError:
+	    return ()
+
+    def get_long_description(self):
+	try:
+	    return unicode(self.__tags['Content:long_description'].strip(),'latin-1', 'replace')
+	except IndexError:
+	    return self.description
+
+    def get_copyright(self):
+	try:
+	    c = unicode(self.__tags['Content:copyright'].strip(), 'latin-1', 'replace')
+	    c = copyright.sub("\U00A9", c)
+	    c = mechanical_copyright.sub("\U00A9", c)
+	    return c
+	except IndexError:
+	    return ""
+
+    def get_title(self):
+	return self.__nullcheck('Content:title')
+
+    def get_is_aggregation(self):
+	return self.__nullcheck('Content:is_aggregation', u'no')
+
+    def get_parent_id(self):
+	try:
+	    return  tuple([unicode(element,'latin-1','replace') for element in self.__tags['Content:product_id'].split('_')])
+	except IndexError:
+	    prodid = self.product_id
+	    if self.is_aggregation == u'no':
+		return prodid[:3]
+	    else:
+		return (prodid[0], prodid[1], prodid[2][:-1])
+
+	return self.__nullcheck('Content:parent_id')
+
+    def get_codec(self):
+	return self.__nullcheck('Content:codec')
+
+    def get_provider(self):
+	return self.__nullcheck('Content:provider')
+
+    def get_short_description(self):
+	try:
+	    return unicode(self.__tags['Content:short_description'].strip(),'latin-1', 'replace')
+	except IndexError:
+	    return self.description
+
+    def get_aggregation_id(self):
+	try:
+	    return tuple([unicode(element,'latin-1','replace') for element in self.__tags['Content:aggregation_id'].split('_')])
+	except IndexError:
+	    return self.parent_id
+
+    def get_description(self):
+	try:
+	    desc = self.__tags['Content:short_description'].strip()
+	    if not desc:
+		return self.title
+	    return unicode(desc,'latin-1', 'replace')
+	except IndexError:
+	    return self.title
+
+    def get_price(self):
+	try:
+	    return float(self.__tags['Content:price'])
+	except ValueError:
+	    return 0.00
+
+    def get_product_id(self):
+	try:
+	    return  tuple([unicode(element,'latin-1','replace') for element in self.__tags['Content:product_id'].split('_')])
+	except IndexError:
+	    return ()
+
+    def get_pub_date_start(self):
+	try:
+	    try:
+		return datetime.date(*(time.strptime(self.__tags['Content:pub_date_start'], '%d-%b-%Y')[0:3]))
+	    except ValueError:
+		return datetime.date(*(time.strptime(self.__tags['Content:pub_date_start'], '%d-%b-%y')[0:3]))
+	except IndexError:
+	    return self.pubdate
+
+    def get_country(self):
+	try:
+	    return int(self.__tags['Content:country'])
+	except ValueError, IndexError:
+	    return 0
+
+    def get_title_id(self):
+	try:
+	    return  tuple([unicode(element,'latin-1','replace') for element in self.__tags['Content:title_id'].split('_')])
+	except IndexError:
+	    return ()
+
+    def get_author(self):
+	author = etal.sub("...",self.__tags['Content:author'])
+	author = AandB.sub(",",author)
+	return tuple([unicode(element.strip(), 'latin-1', 'replace') for element in author.split(',')])
+
+    def get_category_search(self):
+        cats = tuple([element.strip() for element in self.__nullcheck('Content:category_search', '').split(',')])
+	if cats == ('',):
+	    return ()
+	else:
+	    return cats
+
+
+    short_title = property(get_short_title, doc="Docstring for attribute short_title")
+
+    user_alias = property(get_user_alias, doc="Docstring for attribute user_alias")
+
+    category_subcategory_search = property(get_category_subcategory_search, doc="Docstring for attribute category_subcategory_search")
+
+    pubdate = property(get_pubdate, doc="Docstring for attribute pubdate")
+
+    narrator = property(get_narrator, doc="Docstring for attribute narrator")
+
+    parent_title = property(get_parent_title, doc="Docstring for attribute parent_title")
+
+    keywords = property(get_keywords, doc="Docstring for attribute keywords")
+
+    long_description = property(get_long_description, doc="Docstring for attribute long_description")
+
+    copyright = property(get_copyright, doc="Docstring for attribute copyright")
+
+    title = property(get_title, doc="Docstring for attribute title")
+
+    is_aggregation = property(get_is_aggregation, doc="Docstring for attribute is_aggregation")
+
+    parent_id = property(get_parent_id, doc="Docstring for attribute parent_id")
+
+    codec = property(get_codec, doc="Docstring for attribute codec")
+
+    provider = property(get_provider, doc="Docstring for attribute provider")
+
+    short_description = property(get_short_description, doc="Docstring for attribute short_description")
+
+    aggregation_id = property(get_aggregation_id, doc="Docstring for attribute aggregation_id")
+
+    description = property(get_description, doc="Docstring for attribute description")
+
+    price = property(get_price, doc="Docstring for attribute price")
+
+    product_id = property(get_product_id, doc="Docstring for attribute product_id")
+
+    pub_date_start = property(get_pub_date_start, doc="Docstring for attribute pub_date_start")
+
+    country = property(get_country, doc="Docstring for attribute country")
+
+    title_id = property(get_title_id, doc="Docstring for attribute title_id")
+
+    author = property(get_author, doc="Docstring for attribute author")
+
+    category_search = property(get_category_search, doc="Docstring for attribute category_search")
+    
+#if __name__ == "__main__":
+    #aa = aafile(r"F:\Documents\Audio Books\Bernard Cornwell and Susannah Kells\Unknown Album\A Crowning Mercy (Unabridged), Part 1.aa")
+    #for att in aa.__slots__:
+	#print "%s=%r" % (att, getattr(aa,att))
+    
+
+
